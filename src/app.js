3d252691f03012fe47b24b17ad63d0edd5a2b0dc
Daiana Varela <dvarelaal@uoc.edu>
2026-04-03 18:11:04 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//Importo la dependencia de gsap
+import { gsap } from "gsap";
+
+//Hacer que gire el elemento con clase '.js-circular'
+
+gsap.to('.js-circular', {
+  rotation: 360,
+  transformOrigin: "50% 50%",
+  duration: 5,
+  ease: "none",
+  repeat: -1
+})
+
