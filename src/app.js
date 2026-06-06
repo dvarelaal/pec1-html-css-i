3a6a5942fc0d7c564c1c51b1a05a048c664d113b
Daiana Varela <dvarelaal@uoc.edu>
2026-06-06 11:18:54 +0200

    correción código

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 //Importo la dependencia de gsap
 import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
 
 //Hacer que gire el elemento con clase '.js-circular'
 
@@ -12,6 +16,26 @@ gsap.to('.js-circular', {
   repeat: -1
 })
 */
+
+//hacer que ".js-display" salga con una animacion wavy con color al cargar la página
+const split = SplitText.create(".js-display", { type: "chars" });
+
+gsap.from(split.chars, {
+  y: 40, color: "#F27979", opacity: 0,
+  stagger: { each: 0.04, from: "start" },
+  duration: 0.6, ease: "sine.out"
+});
+
+//hacer que las hojas y limones se muevan al azar un poco en index.html
+gsap.to(".js-img-float", {
+  y: "random(-20, 20)",
+  x: "random(-10, 10)",
+  rotation: "random(-15, 15)",
+  duration: "random(2, 4)",
+  ease: "sine.inOut",
+  yoyo: true, repeat: -1
+});
+
 
 
 
